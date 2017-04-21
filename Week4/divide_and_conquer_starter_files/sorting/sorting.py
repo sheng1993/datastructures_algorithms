62d831670f0478fdd636bc504a7a4b5ad78ddb9d
slin <lintw83@gmail.com>
2017-04-21 10:59:20 -0600

    quicksort partition3

--- Week4/divide_and_conquer_starter_files/sorting/sorting.py
+++ Week4/divide_and_conquer_starter_files/sorting/sorting.py
@@ -3,8 +3,19 @@ import sys
 import random
 
 def partition3(a, l, r):
-    #write your code here
-    pass
+    x = a[l]
+    j = l
+    k = l
+    for i in range(l + 1, r + 1):
+        if a[i] < x:
+            j += 1
+            k += 1
+            a[i], a[j] = a[j], a[i]
+        elif a[i] == x:
+            k += 1
+    a[l], a[j] = a[j], a[l]
+    return j, k
+
 
 def partition2(a, l, r):
     x = a[l]
@@ -23,9 +34,9 @@ def randomized_quick_sort(a, l, r):
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
     #use partition3
-    m = partition2(a, l, r)
+    m, m2 = partition3(a, l, r)
     randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    randomized_quick_sort(a, m2 + 1, r);
 
 
 if __name__ == '__main__':
