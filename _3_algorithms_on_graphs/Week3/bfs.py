46e04595c5687935a81007470533dea223c0b744
slin <lintw83@gmail.com>
2018-06-26 20:03:31 -0600

    bfs implementation

--- _3_algorithms_on_graphs/Week3/bfs.py
+++ _3_algorithms_on_graphs/Week3/bfs.py
@@ -1,16 +1,44 @@
-import _3_algorithms_on_graphs.Week1.dfs as graph
 import math
 
+G = {'A': ['B', 'S'],
+     'B': ['A', 'C', 'H', 'G'],
+     'C': ['B', 'S'],
+     'D': ['E', 'F'],
+     'E': ['S', 'D'],
+     'F': ['D', 'G'],
+     'G': ['F', 'H', 'D'],
+     'H': ['B', 'G'],
+     'S': ['A', 'E', 'D', 'C']}
 
-def bfs(G: graph.Graph, S: graph.Vertex):
-    dist = [math.inf] * len(G.vertices)
-    prev = [None] * len(G.vertices)
-    dist[S] = 0
-    Q = [S]
-    while len(Q) != 0:
-        u = Q.pop(0)
-        for n in u.adj: # type: graph.Vertex
-            if dist[n] == math.inf:
-                Q.append(n)
-                dist[n] = dist[u] + 1
-                prev[n] = n
+
+def shortest_path(g: dict, a: str, b: str, path: list = []):
+    q = []
+    dist = dict()
+    prev = dict()
+
+    q.append(a)
+    dist[a] = 0
+
+    while q:
+        v = q.pop()
+        for n in g[v]: # type: str
+            if n not in dist:
+                q.append(n)
+                dist[n] = dist[v] + 1
+                prev[n] = v
+
+    return dist, prev
+
+
+def reconstruct_path(s: str, u: str, prev: dict):
+    result = []
+    while u != s:
+        result.append(u)
+        u = prev[u]
+    result.append(u)
+    return result[::-1]
+
+
+dist, prev = shortest_path(G, 'S', 'A', [])
+print(dist)
+print(reconstruct_path('S', 'F', prev))
